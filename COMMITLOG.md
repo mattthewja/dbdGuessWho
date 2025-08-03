039510de247b777a15bdfaa2979299cc842a2d2f
Ratchakorn Matthew Anuvattanaphorn <179329749+mattthewja@users.noreply.github.com>
2025-08-03 19:22:12 +0700

    - feat: add new character and change mode button along with their corresponding logic. - refactor: make functions with game initialization for repeated use. - feat: put all divs into a vertical flexbox - refactor: change some positioning rules to allow for flexbox to do its job.

---
+++ COMMITLOG.md
@@ -0,0 +1,8 @@
+v.0.1.0
+- feat: add new character and change mode button along with their corresponding logic.
+- refactor: make functions with game initialization for repeated use.
+- feat: put all divs into a vertical flexbox
+- refactor: change some positioning rules to allow for flexbox to do its job.
+
+v.0.0.1
+- initial commit
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// character list
+// character lists
 const killers = [
     "Trapper.png",
     "Wraith.png",
@@ -42,40 +42,90 @@ const killers = [
     "SpringTrap.png"
 ];
 
-// choose random player killer
-const randomIndex = Math.floor(Math.random() * killers.length);
-const chosenKiller = killers[randomIndex];
+const survivors = [
+    "Wesker.png" // for debug
+];
 
-const playerPortrait = document.getElementById("player-portrait");
-playerPortrait.src = `assets/portraits/${chosenKiller}`;
-playerPortrait.alt = `You are ${chosenKiller.replace('.png', '')}`;
+const allCharacters = killers.concat(survivors);
+
+let currentMode = "killer";
+let characters = killers;
+
+//* Game state interaction
+function changeGamemode() {
+    if (currentMode == 'killer') {
+        currentMode = "survivor";
+        characters = survivors;
+    } else if (currentMode == 'survivor') {
+        currentMode = "all";
+        characters = allCharacters;
+    } else {
+        currentMode = 'killer',
+        characters = killers;
+    };
+
+    randomisePlayerCharacter();
+    initialiseBoard();
+};
+
+const changeModeButton = document.getElementById("change-mode");
+changeModeButton.addEventListener("click", () => {
+    changeGamemode();
+    console.log(`Changed to ${currentMode}`);
+})
+
+//* Player portrait
+function randomisePlayerCharacter() {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    const chosenCharacter = characters[randomIndex];
+
+    const playerPortrait = document.getElementById("player-portrait");
+    playerPortrait.src = `assets/portraits/${chosenCharacter}`;
+    playerPortrait.alt = `You are ${chosenCharacter.replace('.png', '')}`;
+};
+
+const newCharacterButton = document.getElementById("change-character");
+newCharacterButton.addEventListener("click", () => {
+    randomisePlayerCharacter(characters);
+
+    document.getElementById("player-info");
+    playerInfo.classList.remove("flipped");
+})
 
 // allow player to hide own portrait
 const playerInfo = document.getElementById("player-info");
 playerInfo.addEventListener("click", () => {
     playerInfo.classList.toggle("flipped");
-})
-
-// populate and logic for hiding portraits //
-const board = document.querySelector(".board");
-
-killers.forEach(file =>{
-    const card = document.createElement("div");
-    card.className = "card";
-
-    const portrait = document.createElement("img");
-    portrait.src = `assets/portraits/${file}`;
-    portrait.className = "portrait"
-
-    const flipped = document.createElement("img");
-    flipped.src = 'assets/icons/portraitFlippedIcon.png';
-    flipped.className = "flipped-img-icon"
-
-    card.appendChild(portrait);
-    card.appendChild(flipped);
-    board.appendChild(card);
-
-    card.addEventListener("click", () => {
-        card.classList.toggle("flipped");
-    });
 });
+
+//* populate and logic for hiding portraits //
+
+function initialiseBoard() {
+    const board = document.querySelector(".board");
+    board.innerHTML = "";
+
+    characters.forEach(file => {
+        const card = document.createElement("div");
+        card.className = "card";
+
+        const portrait = document.createElement("img");
+        portrait.src = `assets/portraits/${file}`;
+        portrait.className = "portrait";
+
+        const flipped = document.createElement("img");
+        flipped.src = `assets/icons/portraitFlippedIcon.png`;
+        flipped.className = "flipped-img-icon";
+
+        card.appendChild(portrait);
+        card.appendChild(flipped);
+        board.appendChild(card);
+
+        card.addEventListener("click", () => {
+            card.classList.toggle("flipped");
+        })
+    })
+}
+
+// Page start setup
+randomisePlayerCharacter();
+initialiseBoard();
